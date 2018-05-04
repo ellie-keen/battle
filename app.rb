20aec00cd67a49a78278d58b6ac78c2a15e64a48
anderscodes <anders.j.persson@hotmail.com>
2018-05-04 11:09:45 +0100

    Skinnified the controller

--- app.rb
+++ app.rb
@@ -7,28 +7,30 @@ class Battle < Sinatra::Base
   enable :sessions
   set :session_secret, "Our secret session"
 
+
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_name_1])
-    $player_2 = Player.new(params[:player_name_2])
-
+    @player_1 = Player.new(params[:player_name_1])
+    @player_2 = Player.new(params[:player_name_2])
+    $game = Game.new(@player_1, @player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_name_1 = $player_1.name
-    @player_name_2 = $player_2.name
-    @player_hit_points_1 = $player_1.health
-    @player_hit_points_2 = $player_2.health
+    @player_name_1 = $game.player_1.name
+    @player_name_2 = $game.player_2.name
+    @player_hit_points_1 = $game.player_1.health
+    @player_hit_points_2 = $game.player_2.health
 
     erb :play
   end
 
   get '/cripplethem' do
-    Game.new($player_1, $player_2).attack($player_2)
+    $game.attack($game.player_2)
 
     erb :cripplethem
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,12 @@ describe Game do
   let(:player_1) { double :player_1 }
   let(:player_2) { double :player_2 }
 
+  describe '#initialize' do
+    it 'should assign the variable player_1' do
+    expect(game.player_1).to eq(player_1)
+  end
+  end
+
   describe '#attack' do
     it "should call decrease health method on argument" do
       expect(player_2).to receive(:decrease_health)
